d4e970ac64f2e1b31cecd0bc697d389a0cbf93ee
ibnutriyuono <ibnutriyuono@gmail.com>
2019-10-14 21:59:17 +0700

    adding data

--- data/arsenal2019.js
+++ data/arsenal2019.js
@@ -26,40 +26,5 @@ const arr = [
     nationality: "Greece",
     thumbnail:
       "https://www.thesportsdb.com/images/media/player/thumb/tx5vqq1544985296.jpg"
-  },
-  {
-    strNumber: "8",
-    name: "Daniel Ceballos",
-    nationality: "Spain",
-    thumbnail:
-      "https://www.thesportsdb.com/images/media/player/thumb/kkuxes1564607623.jpg"
-  },
-  {
-    strNumber: "9",
-    name: "Alexandre Lacazette",
-    nationality: "France",
-    thumbnail:
-      "https://www.thesportsdb.com/images/media/player/thumb/f0i81c1510852903.jpg"
-  },
-  {
-    strNumber: "10",
-    name: "Mesut Ozil",
-    nationality: "Germany",
-    thumbnail:
-      "https://www.thesportsdb.com/images/media/player/thumb/g0xlkp1510859385.jpg"
-  },
-  {
-    strNumber: "11",
-    name: "Lucas Torreira",
-    nationality: "Uruguay",
-    thumbnail:
-      "https://www.thesportsdb.com/images/media/player/thumb/7zm66z1544985889.jpg"
-  },
-  {
-    strNumber: "14",
-    name: "Pierre-Emerick Aubameyang",
-    nationality: "Gabon",
-    thumbnail:
-      "https://www.thesportsdb.com/images/media/player/thumb/fnk3u51520755737.jpg"
   }
 ];
