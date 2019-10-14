f1627732602af9572b87c11b64bca3d19d057463
ibnutriyuono <ibnutriyuono@gmail.com>
2019-10-14 20:40:44 +0700

    remove arsenal array

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,33 +1,4 @@
-let arr = [
-  {
-    strNumber: "1",
-    name: "Bernd Leno",
-    nationality: "Germany",
-    thumbnail:
-      "https://www.thesportsdb.com/images/media/player/thumb/shcmfk1533645517.jpg"
-  },
-  {
-    strNumber: "2",
-    name: "Hector Bellerin",
-    nationality: "Spain",
-    thumbnail:
-      "https://www.thesportsdb.com/images/media/player/thumb/agkw071510851996.jpg"
-  },
-  {
-    strNumber: "3",
-    name: "Kieran Tierney",
-    nationality: "Scotland",
-    thumbnail:
-      "https://www.thesportsdb.com/images/media/player/thumb/an83q01513028545.jpg"
-  },
-  {
-    strNumber: "5",
-    name: "Sokratis Papastathopoulos",
-    nationality: "Greece",
-    thumbnail:
-      "https://www.thesportsdb.com/images/media/player/thumb/tx5vqq1544985296.jpg"
-  }
-];
+console.log(arr);
 const binarySearch = () => {
   let cari = document.getElementById("nomorpunggung").value;
   let start = 0;
