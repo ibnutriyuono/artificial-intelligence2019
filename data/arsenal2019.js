98ea49237bfbab683493228c01050a54fbf6c08d
ibnutriyuono <ibnutriyuono@gmail.com>
2019-10-14 20:40:56 +0700

    adding arsenal array

---
+++ data/arsenal2019.js
@@ -0,0 +1,30 @@
+const arr = [
+  {
+    strNumber: "1",
+    name: "Bernd Leno",
+    nationality: "Germany",
+    thumbnail:
+      "https://www.thesportsdb.com/images/media/player/thumb/shcmfk1533645517.jpg"
+  },
+  {
+    strNumber: "2",
+    name: "Hector Bellerin",
+    nationality: "Spain",
+    thumbnail:
+      "https://www.thesportsdb.com/images/media/player/thumb/agkw071510851996.jpg"
+  },
+  {
+    strNumber: "3",
+    name: "Kieran Tierney",
+    nationality: "Scotland",
+    thumbnail:
+      "https://www.thesportsdb.com/images/media/player/thumb/an83q01513028545.jpg"
+  },
+  {
+    strNumber: "5",
+    name: "Sokratis Papastathopoulos",
+    nationality: "Greece",
+    thumbnail:
+      "https://www.thesportsdb.com/images/media/player/thumb/tx5vqq1544985296.jpg"
+  }
+];
