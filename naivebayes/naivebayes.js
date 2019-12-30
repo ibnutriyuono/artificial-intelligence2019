1227526f95eddbe3249d3e01d7357b7772dc224e
K43SJ <ibnutriyuono@gmail.com>
2019-12-30 21:57:39 +0700

    adding simple validation

--- naivebayes/naivebayes.js
+++ naivebayes/naivebayes.js
@@ -4,47 +4,48 @@ const predict = () => {
     let jumlahpekerja = document.getElementById("jumlahpekerja").value;
     let omzet = document.getElementById("omzet").value;
     let jumlahaset = document.getElementById("jumlahaset").value;
-    fetch(`http://localhost:5000/api/prediksi/?namaukm=${namaukm}&lamakerja=${lamakerja}&jumlahpekerja=${jumlahpekerja}&omzet=${omzet}&jumlahaset=${jumlahaset}`)
-        .then((response) => {
-            return response.json();
-        })
-        .then((dataukm) => {
-            if (dataukm['Hasil'][1] == "Tidak") {
-                Swal.fire({
-                    type: "error",
-                    title: "Maaf",
-                    text: `Penerima Modal pada unit usaha atas nama ${namaukm} ${dataukm['Hasil'][1].toLowerCase()} di terima`
-                });
-            } else if (dataukm['Hasil'][1] == "Tunda") {
-                Swal.fire({
-                    type: "warning",
-                    title: "Maaf",
-                    text: `Penerima Modal pada unit usaha atas nama ${namaukm} masih di ${dataukm['Hasil'][1].toLowerCase()}`
-                });
-            } else {
-                Swal.fire({
-                        type: "success",
-                        title: "Selamat",
-                        text: `Penerima Modal pada unit usaha atas nama ${namaukm} di terima`,
-                        showCancelButton: true,
-                        confirmButtonColor: '#3085d6',
-                        cancelButtonColor: '#d33',
-                        confirmButtonText: 'Lihat detail perhitungan'
-                    })
-                    .then((result) => {
-                        if (result.value) {
-                            fetch(`http://localhost:5000/api/prediksi/data/?namaukm=${namaukm}&lamakerja=${lamakerja}&jumlahpekerja=${jumlahpekerja}&omzet=${omzet}&jumlahaset=${jumlahaset}`)
-                                .then(response => {
-                                    return response.json();
-                                })
-                                .then(data => {
-                                    Swal.fire({
-                                        title: "<i>Detail Data Perhitungan</i>",
-                                        animation: false,
-                                        customClass: {
-                                            popup: "animated fadeInDown faster"
-                                        },
-                                        html: `<div class="col s12 m4">
+    if (namaukm != '' && lamakerja != '' && jumlahpekerja != '' && omzet != '' && jumlahaset != '') {
+        fetch(`http://localhost:5000/api/prediksi/?namaukm=${namaukm}&lamakerja=${lamakerja}&jumlahpekerja=${jumlahpekerja}&omzet=${omzet}&jumlahaset=${jumlahaset}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((dataukm) => {
+                if (dataukm['Hasil'][1] == "Tidak") {
+                    Swal.fire({
+                        type: "error",
+                        title: "Maaf",
+                        text: `Penerima Modal pada unit usaha atas nama ${namaukm} ${dataukm['Hasil'][1].toLowerCase()} di terima`
+                    });
+                } else if (dataukm['Hasil'][1] == "Tunda") {
+                    Swal.fire({
+                        type: "warning",
+                        title: "Maaf",
+                        text: `Penerima Modal pada unit usaha atas nama ${namaukm} masih di ${dataukm['Hasil'][1].toLowerCase()}`
+                    });
+                } else {
+                    Swal.fire({
+                            type: "success",
+                            title: "Selamat",
+                            text: `Penerima Modal pada unit usaha atas nama ${namaukm} di terima`,
+                            showCancelButton: true,
+                            confirmButtonColor: '#3085d6',
+                            cancelButtonColor: '#d33',
+                            confirmButtonText: 'Lihat detail perhitungan'
+                        })
+                        .then((result) => {
+                            if (result.value) {
+                                fetch(`http://localhost:5000/api/prediksi/data/?namaukm=${namaukm}&lamakerja=${lamakerja}&jumlahpekerja=${jumlahpekerja}&omzet=${omzet}&jumlahaset=${jumlahaset}`)
+                                    .then(response => {
+                                        return response.json();
+                                    })
+                                    .then(data => {
+                                        Swal.fire({
+                                            title: "<i>Detail Data Perhitungan</i>",
+                                            animation: false,
+                                            customClass: {
+                                                popup: "animated fadeInDown faster"
+                                            },
+                                            html: `<div class="col s12 m4">
                                             <div class="card">
                                                 <div class="card-content">
                                                     <p>
@@ -78,12 +79,19 @@ const predict = () => {
                                                 </div>
                                             </div>
                                         </div>`
+                                        })
                                     })
-                                })
-                        }
-                    })
-            }
+                            }
+                        })
+                }
+            });
+    } else {
+        Swal.fire({
+            type: "error",
+            title: "Maaf",
+            text: `isi semua input yang telah disediakan`
         });
+    }
 };
 
 const getdata = () => {
@@ -112,3 +120,9 @@ const getdata = () => {
         console.log('kekw2')
     }
 }
+
+let namaukm = document.getElementById("namaukm").value;
+let lamakerja = document.getElementById("lamakerja").value;
+let jumlahpekerja = document.getElementById("jumlahpekerja").value;
+let omzet = document.getElementById("omzet").value;
+let jumlahaset = document.getElementById("jumlahaset").value;
