764cb5a90205d92a0986827b43ad35204ebe9599
K43SJ <ibnutriyuono@gmail.com>
2019-12-31 14:03:47 +0700

    downloadble csv file

--- naivebayes/naivebayes.js
+++ naivebayes/naivebayes.js
@@ -283,9 +283,12 @@ const getHistory = () => {
             }
             let divContainer = document.getElementById("historydata");
             divContainer.innerHTML = "<h3>History</h3>";
-            divContainer.innerHTML += `<form id="getcsv" action="javascript:getCSV();">
-                                        <input type="submit" id="getcsvbutton" class="btn" value="Download CSV" />
-                                    </form>`;
+            // divContainer.innerHTML += `<form id="getcsv">
+            //                             <input type="submit" id="getcsvbutton" class="btn" value="Download CSV" />
+            //                         </form>`;
+            divContainer.innerHTML += `<a href='#' class="btn" 
+                                            onclick='downloadCSV({ filename: "data-training.csv" });'
+                                        >Download CSV</a>`;
             divContainer.appendChild(table);
         }
     } else {
@@ -295,6 +298,52 @@ const getHistory = () => {
     }
 }
 
-getCSV = () => {
-    console.log('getcsv kekw')
+getCSV = (args) => {
+    var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+    data = args.data || null;
+    if (data == null || !data.length) {
+        return null;
+    }
+
+    columnDelimiter = args.columnDelimiter || ',';
+    lineDelimiter = args.lineDelimiter || '\n';
+
+    keys = Object.keys(data[0]);
+
+    result = '';
+    result += keys.join(columnDelimiter);
+    result += lineDelimiter;
+
+    data.forEach(function (item) {
+        ctr = 0;
+        keys.forEach(function (key) {
+            if (ctr > 0) result += columnDelimiter;
+
+            result += item[key];
+            ctr++;
+        });
+        result += lineDelimiter;
+    });
+    return result;
+}
+
+downloadCSV = (args) => {
+    var data, filename, link;
+    var csv = getCSV({
+        data: history
+    });
+    if (csv == null) return;
+
+    filename = args.filename || 'export.csv';
+
+    if (!csv.match(/^data:text\/csv/i)) {
+        csv = 'data:text/csv;charset=utf-8,' + csv;
+    }
+    data = encodeURI(csv);
+
+    link = document.createElement('a');
+    link.setAttribute('href', data);
+    link.setAttribute('download', filename);
+    link.click();
 }
